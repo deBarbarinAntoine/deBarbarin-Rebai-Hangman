bfb15b877fc635e44682e09956855fe0e6d4306e
rihem <rihem.rebai@ynov.com>
2023-10-16 08:37:49 +0200

    proposition mot et revelation lettres trouvees

--- exec/funcs.go
+++ exec/funcs.go
@@ -1,1 +1,24 @@
 package main
+
+import "fmt"
+
+
+func CheckWord(word string) {
+	var input string
+	fmt.Print("Rentre un mot : ")
+	fmt.Scanln(&input)
+	if input == word {
+		fmt.Print("Mot identique.")
+	} else {
+		fmt.Print("Mot non identique.")
+	}
+}
+
+func displayWord (word []rune, wordDisplay []rune, char rune) []rune { 
+	for i, r := range word {
+		if r == char {
+			wordDisplay[i] = char
+		}
+	}
+	return wordDisplay
+} 
